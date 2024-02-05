90479544d568caabe58cc3e06cdb25df485dc87b
Dmitryra27 <dmitryra27@gmail.com>
2024-02-05 23:41:19 +0300

    Доработка модели User

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,14 +1,31 @@
 import { Schema, Document, model } from 'mongoose';
 import { User } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
-  name:String,
-  email:String,
-  avatarPath:String,
+  name:{
+    type: String,
+    unique: true,
+    required: true,
+    minlength: [2, 'Min length for firstname is 2']
+  },
+  email:{
+    type: String,
+    unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
+  },
+  avatarPath:{
+    type: String,
+    minlength: [5, 'Min length for avatar path is 5'],
+    required: false,
+  },
   password:String,
   userType:String,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
