956e026acc5993f86831d0299fd97a2ae171fd24
Dmitryra27 <dmitryra27@gmail.com>
2024-02-17 10:59:01 +0300

    Реализация Comment Service

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,13 +5,16 @@ import { Component } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import {createOfferContainer} from './shared/modules/offer/index.js';
 import {createUserContainer} from './shared/modules/user/user.container.js';
+import { createCommentContainer } from './shared/modules/comment/index.js';
 
 
 async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
-    createOfferContainer(),);
+    createOfferContainer(),
+    createCommentContainer()
+    );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
