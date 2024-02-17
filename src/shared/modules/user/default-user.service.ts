6efa0dd6dbb865ebd54ac4c8c0803dd38cb523ca
Dmitryra27 <dmitryra27@gmail.com>
2024-02-17 11:05:37 +0300

    Расширит интерфейс UserService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -5,6 +5,7 @@ import pino from 'pino';
 import Logger = pino.Logger;
 import {inject, injectable} from 'inversify';
 import {Component} from '../../types/index.js';
+import { UpdateUserDto } from './dto/update-user.dto.js';
 
 @injectable()
 export class DefaultUserService implements UserService {
@@ -35,5 +36,11 @@ export class DefaultUserService implements UserService {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel
+      .findByIdAndUpdate(userId, dto, { new: true })
+      .exec();
   }
 }
