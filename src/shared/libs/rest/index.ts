37c663a809d3df3301840a4fb3122cd89a4d85b9
Dmitryra27 <dmitryra27@gmail.com>
2024-02-23 19:30:54 +0300

    Создан exception-filter

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -2,3 +2,5 @@ export * from './route/http-method.enum.js';
 export * from './route/route.interface.js';
 export * from './controller/controller.interface.js';
 export * from './controller/base-controller.abstract.js';
+export * from './exception-filter/exception-filter.interface.js';
+export * from './exception-filter/app-exception-filter.js';
