13723f96b9398765ff7cfaa27cd900cac9267431
Dmitryra27 <dmitryra27@gmail.com>
2024-02-23 22:49:04 +0300

    Refactor UserController

--- src/shared/libs/rest/controller/controller.interface.ts
+++ src/shared/libs/rest/controller/controller.interface.ts
@@ -1,9 +1,16 @@
-import { Response, Router } from 'express';
-import { Route } from '../route/route.interface.js';
+import e, { Response } from 'express';
+import {HttpMethod} from "../route/http-method.enum.js";
 
 export interface Controller {
-  readonly router: Router;
-  addRoute(route: Route): void;
+  //readonly router: Router;
+  //addRoute(route: Route): void;
+
+  addRoute(route: {
+    path: string;
+    handler: ({body}: any, res: e.Response) => Promise<void>;
+    method: HttpMethod
+  }): void;
+
   send<T>(res: Response, statusCode: number, data: T): void;
   ok<T>(res: Response, data: T): void;
   created<T>(res: Response, data: T): void;
--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -2,3 +2,5 @@ export * from './user.entry.js';
 export * from './dto/create-user.dto.js';
 export * from './default-user.service.js';
 export * from './user.controller.js';
+export * from './rdo/create-user.rdo.js';
+export * from './dto/check-user-status.dto.js';
