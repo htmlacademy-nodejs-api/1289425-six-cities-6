43ac278378fe952fb54d59cbfdba9e223b37d570
Dmitryra27 <dmitryra27@gmail.com>
2024-02-28 00:15:52 +0300

    Внедрена валидация User Module

--- src/shared/modules/user/dto/create-user.dto.ts
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+import { CreateUserMessages } from './create-user.messages.js';
+
 export class CreateUserDto {
-  public name:string = '';
-  public email:string = '';
-  public avatarPath?:string;
-  public password:string = '';
-  public userType:string = '';
+  @IsEmail({}, { message: CreateUserMessages.email.invalidFormat })
+  public email!: string;
+
+  @IsString({ message: CreateUserMessages.name.invalidFormat })
+  @Length(1, 15, { message: CreateUserMessages.name.lengthField })
+  public name!: string;
+
+  @IsString({ message: CreateUserMessages.userType.invalidFormat })
+  @Length(1, 15, { message: CreateUserMessages.userType.lengthField })
+  public userType!: string;
+
+  @IsString({ message: CreateUserMessages.password.invalidFormat })
+  @Length(6, 12, { message: CreateUserMessages.password.lengthField })
+  public password!: string;
+
+  public avatarPath?: string;
 }
--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -4,3 +4,5 @@ export * from './default-user.service.js';
 export * from './user.controller.js';
 export * from './rdo/create-user.rdo.js';
 export * from './dto/check-user-status.dto.js';
+export * from './dto/create-user.messages.js';
+export * from './dto/login-user.messages.js';
